14fa0d522a94633905d2f07990e8a383eb7668c1
Elham Amini <elhamfarvid@gmail.com>
2019-10-13 20:56:28 -0400

    changed

--- family-tree.js
+++ family-tree.js
@@ -1,77 +1,63 @@
 class FamilyTree {
-  
-      constructor(value) {
-        if (!value || typeof value !== "string") {
-          throw "error";
+  constructor(value) {
+    if (!value || typeof value !== "string") {
+      throw "error";
+    }
+    this.value = value;
+    this.children = [];
+  }
+  insert(value) {
+    if (value) {
+      this.children.push(new FamilyTree(value));
+    }
+  }
+  familySize() {
+    return this.children.length + 1;
+  }
+  findMember(member) {
+    if (!member) {
+      return undefined;
+    }
+    memberFinder(this);
+    function memberFinder(node) {
+      if (!node) {
+        return undefined;
+      }
+      if (node.value === member) {
+        return node;
+      }
+      if (!node.children) {
+        return undefined;
+      }
+
+      for (let i = 0; i < node.children.length; i++) {
+        let nodeFound = memberFinder(node.children[i]);
+        if (nodeFound) {
+          return nodeFound;
         }
-        this.value = value;
-        this.children = [];
-      }
-      insert(value) {
-        if (value) {
-          this.children.push(new FamilyTree(value));
-        }
-      }
-      familySize() {
-        return this.children.length + 1;
-      }
-      findMember(member) {
-        if (!member) {
-          return undefined;
-        }
-        return function memberFinder(node = this) {
-          if (!node) {
-            return undefined;
-          }
-          if (node.value === member) {
-            return node;
-          }
-          if (!node.children) {
-            return undefined;
-          }
-          node.children.forEach(child => {
-            const nodeFound = memberFinder(child);
-            if (nodeFound) {
-              return nodeFound;
-            }
-          });
-          return undefined;
-        };
-        
-      }
-      log() {
-        const arr = [];
-        let generation = 1;
-        let str = "";
-    
-        const firstObj = new FamilyTree(this.value);
-    
-        const arrMaker = (obj = firstObj) => {
-          Object.values(obj).forEach(val => {
-            if (Array.isArray(val)) {
-              generation += 1;
-              val.forEach(v => {
-                arrMaker(v);
-              });
-            } else {
-              for (let i = 0; i < generation; i++) {
-                str += "--";
-              }
-              str += val;
-              arr.push(str);
-            }
-          });
-          return arr;
-        };
-        return [arrMaker().join(",")];
-        // console.log("**", this.children[1].children);
-        // return `-- ${this.value}`;
       }
     }
-    
-    // tree traversal - depth first search
-    
- 
+    return undefined;
+  }
+  log() {
+    let counter = 1;
+    let str = "--";
+    let returnStr = "";
+
+    return function myLog(node = this) {
+      returnStr = counter * str + node.value;
+      if (node.children) {
+        for (const child of node.children) {
+          return myLog(child);
+        }
+      }
+
+      console.log(returnStr);
+    }; // console.log("**", this.children[1].children);
+    // return `-- ${this.value}`;
+  }
 }
+
+// tree traversal - depth first search
 
 module.exports = FamilyTree;
